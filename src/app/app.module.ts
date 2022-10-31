73a247e872b228efc6efc3cd1fa070272455e9ba
Veronica Zarate <veronicazarate7826@gmailc.com>
2022-10-31 17:10:17 -0300

    agrego imagenes logo y redes sociales. Agrego componentes piedepagina, banner, proyectos, hys, estudios, header, acerca-de, experiencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { PiedepaginaComponent } from './components/piedepagina/piedepagina.component';
+import { HysComponent } from './components/hys/hys.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciasComponent,
+    EstudiosComponent,
+    ProyectosComponent,
+    PiedepaginaComponent,
+    HysComponent
   ],
   imports: [
     BrowserModule,
